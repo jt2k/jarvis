8a902ef6085626ffe843b559ab93444479506eec
Jason Tan <git@jasontan.org>
2014-06-28 17:20:20 -0500

    Geocode: Add reverse geocoding

--- responders/GeocodeResponder.php
+++ responders/GeocodeResponder.php
@@ -7,16 +7,29 @@ class GeocodeResponder extends Responder
 
     public function respond()
     {
-        $address = urlencode(trim($this->matches[1]));
-        $url = "http://maps.googleapis.com/maps/api/geocode/json?address={$address}&sensor=false";
-        $obj = $this->request($url);
+        $query = trim($this->matches[1]);
+        if (preg_match('/^(-?[0-9\.]+)[ ,]+(-?[0-9\.]+)$/', $query, $m)) {
+            $param = 'latlng';
+        } else {
+            $param = 'address';
+        }
+        $query = urlencode($query);
+        $url = "https://maps.googleapis.com/maps/api/geocode/json?{$param}={$query}&sensor=false";
+        $obj = $this->request($url, 3600*24, 'geocode');
         if (!is_object($obj) || $obj->status !== 'OK') {
             return 'Not found';
         }
-        if (!is_array($obj->results) || !isset($obj->results[0]) || !isset($obj->results[0]->geometry->location)) {
+        if (!is_array($obj->results) || !isset($obj->results[0])) {
             return 'Not found';
         }
 
-        return "{$obj->results[0]->geometry->location->lat}, {$obj->results[0]->geometry->location->lng}";
+        if ($param == 'address') {
+            return "{$obj->results[0]->geometry->location->lat}, {$obj->results[0]->geometry->location->lng}";
+        } else {
+            $full_address = $obj->results[0]->formatted_address;
+            $full_address = preg_replace('/^[^,]+, /', '', $full_address);
+            $full_address = preg_replace('/, USA$/', '', $full_address);
+            return $full_address;
+        }
     }
 }

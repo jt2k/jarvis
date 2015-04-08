f01fe420b26e038a925fe516eeb6223149e332b5
Jason Tan <jason.tan@vanderbilt.edu>
2015-04-08 10:28:44 -0500

    SPC: Add fire outlook

--- responders/SPCResponder.php
+++ responders/SPCResponder.php
@@ -3,23 +3,27 @@ namespace jt2k\Jarvis;
 
 class SPCResponder extends Responder
 {
-    public static $pattern = '^(tornado|hail|wind)';
-    protected $endpoint = 'http://www.spc.noaa.gov/products/outlook';
+    public static $pattern = '^(tornado|hail|wind|fire)';
+    protected $endpoint = 'http://www.spc.noaa.gov/products';
 
     public function respond()
     {
         $type = $this->matches[1];
         switch (strtolower($type)) {
             case 'tornado':
-                $url = "{$this->endpoint}/day1probotlk_1300_torn.gif";
+                $url = "{$this->endpoint}/outlook/day1probotlk_1300_torn.gif";
                 break;
 
             case 'hail':
-                $url = "{$this->endpoint}/day1probotlk_1300_hail.gif";
+                $url = "{$this->endpoint}/outlook/day1probotlk_1300_hail.gif";
                 break;
 
             case 'wind':
-                $url = "{$this->endpoint}/day1probotlk_1300_wind.gif";
+                $url = "{$this->endpoint}/outlook/day1probotlk_1300_wind.gif";
+                break;
+
+            case 'fire':
+                $url = "{$this->endpoint}/fire_wx/day1otlk_fire.gif";
                 break;
 
             default:

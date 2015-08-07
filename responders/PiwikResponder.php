55e19c66542e6d05415970146b335cf1c147a789
Jason Tan <jason.tan@vanderbilt.edu>
2015-08-07 12:28:41 -0500

    Add Piwik responder

---
+++ responders/PiwikResponder.php
@@ -0,0 +1,40 @@
+<?php
+namespace jt2k\Jarvis;
+
+class PiwikResponder extends Responder
+{
+    public static $pattern = '^piwik (\w+)$';
+
+    public function respond()
+    {
+        if (!$this->requireConfig(array('piwik'))) {
+            return 'piwik not configured';
+        }
+
+        $config = $this->config['piwik'];
+        if (!isset($config['url']) || !isset($config['token']) || !is_array($config['sites'])) {
+            return 'piwik not configured';
+        }
+
+        $site = strtolower($this->matches[1]);
+        if (!array_key_exists($site, $config['sites'])) {
+            return "{$site} not configured";
+        }
+
+        $siteId = urlencode($config['sites'][$site]);
+
+        $url = "{$config['url']}?module=API&method=VisitsSummary.getVisits&idSite={$siteId}&date=last8&period=day&format=json&token_auth={$config['token']}";
+
+        $stats = (array)$this->request($url);
+        $today = array_pop($stats);
+        $week = array_sum($stats);
+        $yesterday = end($stats);
+        return sprintf(
+            "%s visits\nToday (so far): %s\nYesterday: %s\nLast week: %s",
+            $site,
+            number_format($today),
+            number_format($yesterday),
+            number_format($week)
+        );
+    }
+}

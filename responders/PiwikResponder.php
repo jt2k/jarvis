ced65ffa7656efd87e32d4b08454f9e8e5535de5
Jason Tan <jason.tan@vanderbilt.edu>
2015-09-25 14:10:24 -0500

    Piwik: Handle API error

--- responders/PiwikResponder.php
+++ responders/PiwikResponder.php
@@ -26,6 +26,9 @@ class PiwikResponder extends Responder
         $url = "{$config['url']}?module=API&method=VisitsSummary.getVisits&idSite={$siteId}&date=last8&period=day&format=json&token_auth={$config['token']}";
 
         $stats = (array)$this->request($url);
+        if (isset($stats['result']) && $stats['result'] == 'error') {
+            return 'API error. Make sure site is configured correctly and token user has access.';
+        }
         $today = array_pop($stats);
         $week = array_sum($stats);
         $yesterday = end($stats);

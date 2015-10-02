13155426fed0d24a2610d52ef40447066b123402
Jason Tan <jason.tan@vanderbilt.edu>
2015-10-02 12:58:38 -0500

    SPC: Add hurricane outlook (from NHC)

--- responders/SPCResponder.php
+++ responders/SPCResponder.php
@@ -3,7 +3,7 @@ namespace jt2k\Jarvis;
 
 class SPCResponder extends Responder
 {
-    public static $pattern = '^(tornado|hail|wind|fire)$';
+    public static $pattern = '^(tornado|hail|wind|fire|hurricane)$';
     protected $endpoint = 'http://www.spc.noaa.gov/products';
 
     public function respond()
@@ -24,6 +24,11 @@ class SPCResponder extends Responder
 
             case 'fire':
                 $url = "{$this->endpoint}/fire_wx/day1otlk_fire.gif";
+                break;
+
+            // Including hurricane outlook from NHC despite not coming from the SPC
+            case 'hurricane':
+                $url = "http://www.nhc.noaa.gov/xgtwo/two_atl_2d0.png";
                 break;
 
             default:

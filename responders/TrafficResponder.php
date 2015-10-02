248f758775b57e2061b4619df49f9c613cd2bc33
Jason Tan <git@jasontan.org>
2015-10-01 20:51:07 -0500

    Add Traffic responder

---
+++ responders/TrafficResponder.php
@@ -0,0 +1,21 @@
+<?php
+namespace jt2k\Jarvis;
+
+class TrafficResponder extends Responder
+{
+    public static $pattern = '^traffic$';
+
+    public function respond()
+    {
+        if (!$this->requireConfig(array('bingmaps_key', 'location'))) {
+            return 'bingmaps_key and location are required.';
+        }
+
+        $url = 'http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/'
+            . "{$this->config['location'][0]},{$this->config['location'][1]}"
+            . '/12?mapSize=600,400&mapLayer=TrafficFlow&format=png'
+            . "&key={$this->config['bingmaps_key']}";
+
+        return $this->requestProxy($url, 300, 'traffic', 'png') . '?' . time();
+    }
+}
